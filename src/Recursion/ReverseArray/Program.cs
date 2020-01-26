bd8d3ab46145241908ad5a5fa8901e6f4da59e6b
Mihail <mihogv@gmail.com>
2020-01-26 12:12:33 +0200

    Implemented ReverseArray recursive.

---
+++ src/Recursion/ReverseArray/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace ReverseArray
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            var array = new int[] { 1, 2, 3, 4, 5, 6 };
+
+            var reversedArray = ReverseArray(array);
+
+            Console.WriteLine(string.Join(", ", reversedArray)); // output : 6, 5, 4, 3, 2, 1
+        }
+
+        public static int[] ReverseArray(int[] array)
+        {
+            var reversedArray = new List<int>();
+
+            Reverse(array, reversedArray, 0);
+
+            return reversedArray.ToArray();
+        }
+
+        private static void Reverse(int[] currentArray, List<int> result, int index)
+        {
+            if (index == currentArray.Length)
+            {
+                return;
+            }
+
+            Reverse(currentArray, result, index + 1);
+            result.Add(currentArray[index]);
+        }
+    }
+}
