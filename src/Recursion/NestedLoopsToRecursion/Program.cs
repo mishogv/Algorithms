9448c7402ce3b6e6edc8e7ed1537fc994cfbe7e9
Mihail <mihogv@gmail.com>
2020-01-26 12:44:53 +0200

    Implemented nested loops with recursion.

---
+++ src/Recursion/NestedLoopsToRecursion/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace NestedLoopsToRecursion
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            var n = 3;
+
+            NestedLoopsWithRecursion(n);
+        }
+
+        private static void NestedLoopsWithRecursion(int n)
+        {
+            PrintNumbers(n, 0, new int[n]);
+        }
+
+        private static void PrintNumbers(int n, int index, int[] result)
+        {
+            if (index == result.Length)
+            {
+                Console.WriteLine(string.Join(" ", result));
+            }
+            else
+            {
+                for (int i = 1; i <= n; i++)
+                {
+                    result[index] = i;
+                    PrintNumbers(n, index + 1, result);
+                }
+            }
+        }
+    }
+}
