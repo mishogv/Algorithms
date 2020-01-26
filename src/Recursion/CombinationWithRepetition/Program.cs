31d0e8489ecc0989787b1b6b33a83870f0a6b020
Mihail <mihogv@gmail.com>
2020-01-26 13:53:33 +0200

    Implemented CombinationWtihRepetiton problem.

---
+++ src/Recursion/CombinationWithRepetition/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace CombinationWithRepetition
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            var n = 3;
+            var k = 2;
+
+            PrintAllCombinations(n, k);
+        }
+
+        public static void PrintAllCombinations(int n, int k)
+        {
+            PrintAllCombinations(n, k, new int[k]);
+        }
+
+        private static void PrintAllCombinations(int n, int k, int[] result, int index = 0, int element = 1)
+        {
+            if (index == result.Length)
+            {
+                Console.WriteLine(string.Join(" ", result));
+                return;
+            }
+            else
+            {
+                for (int i = element; i <= n; i++)
+                {
+                    result[index] = i;
+                    PrintAllCombinations(n, k, result, index + 1, i);
+                }
+            }
+        }
+    }
+}
