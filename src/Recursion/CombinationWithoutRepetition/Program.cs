8ea6c2b802d4d0d7a342cfcf1e6fc314daae964b
Mihail <mihogv@gmail.com>
2020-02-01 12:16:16 +0200

    Implemented CombinationWithoutRepetition recursive

---
+++ src/Recursion/CombinationWithoutRepetition/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace CombinationWithoutRepetition
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            int n = 3;
+            int k = 2;
+
+            PrintAllCombinations(n, k);
+        }
+
+        public static void PrintAllCombinations(int n, int k)
+        {
+            PrintAllCombinations(n, k, new int[k]);
+        }
+
+        private static void PrintAllCombinations(int n, int k, int[] result, int index = 0, int element = 1)
+        {
+            if (index == result.Length)
+            {
+                Console.WriteLine(string.Join(" ", result));
+                return;
+            }
+            else
+            {
+                for (int i = element; i <= n; i++)
+                {
+                    result[index] = i;
+                    PrintAllCombinations(n, k, result, index + 1, i + 1);
+                }
+            }
+        }
+    }
+}
